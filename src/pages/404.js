a1e73d9d00fa49b93a49ccda1d030a2503df1f36
Marcus Stewart <MMSStewart20@gmail.com>
2020-09-24 20:46:16 -0500

    Added more text to 404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div class="outline">
+      <SEO title="404: Page Not Found" />
+      <h1 style={{ fontSize: `40px` }}>404 :(</h1>
+      <h2>SORRY, THIS PAGE DOES NOT EXIST</h2>
+      <p>
+        Maybe the page you're looking for is somewhere else...
+        <br />
+        You can always go back to the <a href="/">homepage</a>
+      </p>
+    </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
