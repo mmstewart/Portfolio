70b6af1a0554454b33b780e874a881b35e0c4e6d
Marcus Stewart <MMSStewart20@gmail.com>
2020-09-26 14:29:38 -0500

    Fix Grammar Error/Added Color to Name/Format Fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react";
+import { Link } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import "../components/layout.css"
-import "../utils/fontawesome"
+import "../components/layout.css";
+import "../utils/fontawesome";
 
 /* Active Link Color/Text Decoration */
 const activeStyles = {
   color: `#111`,
   textDecoration: `underline`,
   fontFamily: `Raleway, sans-serif`,
-}
+};
 
-const ListLink = props => (
+const ListLink = (props) => (
   <li style={{ padding: 14 }}>
     <Link
       class="cl-effect-17"
@@ -24,7 +24,7 @@ const ListLink = props => (
       {props.children}
     </Link>
   </li>
-)
+);
 
 export default function Layout({ children }) {
   return (
@@ -32,18 +32,18 @@ export default function Layout({ children }) {
       <div class="sidebar" id="left">
         <nav>
           <header>
-            <h2>Marcus Stewart</h2>
+            <h2 id="main-color">Marcus Stewart</h2>
             <div class="sub-sidebar">
               Front-end Geek.<br></br>
               Software Developer.<br></br>
-              Film Photographer Lover.
+              Film Photography Lover.
             </div>
-              <ul>
-                <ListLink to="/">Home</ListLink>
-                <ListLink to="/skills/">Skills</ListLink>
-                <ListLink to="/portfolio/">Portfolio</ListLink>
-                <ListLink to="/resume/">Resume</ListLink>
-              </ul>
+            <ul>
+              <ListLink to="/">Home</ListLink>
+              <ListLink to="/skills/">Skills</ListLink>
+              <ListLink to="/portfolio/">Portfolio</ListLink>
+              <ListLink to="/resume/">Resume</ListLink>
+            </ul>
             <div class="contact">
               Questions? Comments? Concerns? Contact me at:{" "}
               <a href="mailto:mss.stewart1@gmail.com">mss.stewart1@gmail.com</a>
@@ -103,5 +103,5 @@ export default function Layout({ children }) {
       </div>
       {children}
     </div>
-  )
+  );
 }
