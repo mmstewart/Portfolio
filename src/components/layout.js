f935fcd56870c3cfeffd7545182a46dd09f7b388
Marcus Stewart <MMSStewart20@gmail.com>
2020-09-29 16:52:46 -0500

    Updated Layout for Side Header, Added Top Header for Mobile

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Footer from "./footer.js";
 
 import "../components/layout.css";
 import "../utils/fontawesome";
@@ -15,8 +16,6 @@ const activeStyles = {
 const ListLink = (props) => (
   <li style={{ padding: 14 }}>
     <Link
-      class="cl-effect-17"
-      id="cl-effect-17"
       style={{ textDecoration: `none`, color: `rgb(100, 100, 100)` }}
       to={props.to}
       activeStyle={activeStyles}
@@ -28,22 +27,50 @@ const ListLink = (props) => (
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div style={{ outline: `none` }} tabIndex="-1">
+      <div class="navbar">
+        <div>
+          <div>
+            <span>+</span>
+          </div>
+          <div>
+            <h2 id="main-color">Marcus Stewart</h2>
+          </div>
+          <div>
+            <ListLink to="/">Home</ListLink>
+          </div>
+        </div>
+      </div>
       <div class="sidebar" id="left">
         <nav>
           <header>
             <h2 id="main-color">Marcus Stewart</h2>
             <div class="sub-sidebar">
-              Front-end Geek.<br></br>
-              Software Developer.<br></br>
+              Front-end Geek.
+              <br />
+              Software Developer.
+              <br />
               Film Photography Lover.
             </div>
-            <ul>
-              <ListLink to="/">Home</ListLink>
-              <ListLink to="/skills/">Skills</ListLink>
-              <ListLink to="/portfolio/">Portfolio</ListLink>
-              <ListLink to="/resume/">Resume</ListLink>
-            </ul>
+            <div class="lens">
+              <nav class="effect">
+                <ul>
+                  <ListLink to="/">
+                    <span id="ln">Home</span>
+                  </ListLink>
+                  <br />
+                  <ListLink to="/skills/">
+                    <span id="ln">Skills</span>
+                  </ListLink>
+                  <ListLink to="/portfolio/">
+                    <span id="ln">Portfolio</span>
+                  </ListLink>
+                  <ListLink to="/resume/">
+                    <span id="ln">Resume</span>
+                  </ListLink>
+                </ul>
+              </nav>
+            </div>
             <div class="contact">
               Questions? Comments? Concerns? Contact me at:{" "}
               <a href="mailto:mss.stewart1@gmail.com">mss.stewart1@gmail.com</a>
@@ -102,6 +129,7 @@ export default function Layout({ children }) {
         </nav>
       </div>
       {children}
+      <Footer />
     </div>
   );
 }
